72ae434600fefa553742c58839a2ccc0f8734139
Mikael Gabryel Flor de Lis <gabryel.flordelis@gmail.com>
2024-01-18 01:36:18 +0400

    removed form text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -164,10 +164,7 @@ function App() {
             <Form.Group className="mb-3" controlId="formBasicUsername">
               <Form.Label>Username</Form.Label>
               <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
+             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
